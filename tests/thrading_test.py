65279a8a85ff2b7d0e71a0464f9508f9499d3ab5
aki <mark@aur.ll.to>
2024-10-19 19:43:47 +0900

    .

---
+++ tests/thrading_test.py
@@ -0,0 +1,13 @@
+"""テストコード。"""
+
+import pytilpack.threading_
+
+
+def test_parallel():
+    assert pytilpack.threading_.parallel(
+        [lambda x=x: x + 1 for x in range(3)]  # type: ignore[misc]
+    ) == [1, 2, 3]
+
+
+def test_parallel_for():
+    assert pytilpack.threading_.parallel_for(lambda x: x + 1, 3) == [1, 2, 3]
