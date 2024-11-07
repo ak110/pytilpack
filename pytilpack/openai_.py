c812bddb5ecb41d530ce05ab1c8af7e125386628
aki <mark@aur.ll.to>
2024-11-08 08:21:47 +0900

    .

--- pytilpack/openai_.py
+++ pytilpack/openai_.py
@@ -76,12 +76,10 @@ def _make_choice(
     message = openai.types.chat.ChatCompletionMessage.model_construct()
 
     if len(roles := remove_none(c.delta.role for c in choice_list)) > 0:
-        if len(roles) > 1:
-            _warn(strict, f"roleが複数存在します。最後のroleを使用します。{roles=}")
-        message.role = roles[-1]  # type: ignore
+        message.role = _equals_all_get(strict, "role", roles, "assistant")  # type: ignore
 
-    if len(content := remove_none(c.delta.content for c in choice_list)) > 0:
-        message.content = "".join(content)
+    if len(contents := remove_none(c.delta.content for c in choice_list)) > 0:
+        message.content = "".join(contents)
 
     if (
         len(function_calls := remove_none(c.delta.function_call for c in choice_list))
@@ -97,12 +95,7 @@ def _make_choice(
     )
 
     if len(finish_reasons := remove_none(c.finish_reason for c in choice_list)) > 0:
-        if len(finish_reasons) > 1:
-            _warn(
-                strict,
-                f"finish_reasonが複数存在します。最後のfinish_reasonを使用します。{finish_reasons=}",
-            )
-        choice.finish_reason = finish_reasons[-1]  # type: ignore
+        choice.finish_reason = _equals_all_get(strict, "finish_reason", finish_reasons)  # type: ignore
 
     if len(logprobs_list := remove_none(c.logprobs for c in choice_list)) > 0:
         if len(logprobs_list) > 1:
