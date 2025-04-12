83542142c99974298654e04793ddcca7d0e00aaf
aki. <mark@aur.ll.to>
2025-04-13 04:58:49 +0900

    .

--- pytilpack/pathlib_.py
+++ pytilpack/pathlib_.py
@@ -14,9 +14,9 @@ def delete_file(path: str | pathlib.Path) -> None:
 
 
 def get_size(path: str | pathlib.Path) -> int:
-    """ファイル・ディレクトリのサイズを返す。エラー発生時は対象は0扱いとする。"""
-    path = pathlib.Path(path)
+    """ファイル・ディレクトリのサイズを返す。"""
     try:
+        path = pathlib.Path(path)
         if path.is_file():
             try:
                 return path.stat().st_size
@@ -29,10 +29,9 @@ def get_size(path: str | pathlib.Path) -> int:
                 for child in path.iterdir():
                     # 再帰的に子要素のサイズを加算する
                     total_size += get_size(child)
-                return total_size
             except Exception:
                 logger.warning(f"ディレクトリサイズ取得失敗: {path}", exc_info=True)
-                return 0
+            return total_size
         else:
             return 0
     except Exception:
