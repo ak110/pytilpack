15ab156d71e88d574b874d4b4fb79a7f4b5361f8
aki. <mark@aur.ll.to>
2025-04-29 16:53:13 +0900

    .

--- pytilpack/fastapi_.py
+++ pytilpack/fastapi_.py
@@ -2,19 +2,28 @@
 
 import io
 import logging
+import pathlib
 import typing
 
 import httpx
 
+import pytilpack.pytest_
+import pytilpack.web
+
 logger = logging.getLogger(__name__)
 
 
-def assert_bytes(response: httpx.Response, status_code: int = 200) -> bytes:
+def assert_bytes(
+    response: httpx.Response,
+    status_code: int = 200,
+    content_type: str | typing.Iterable[str] | None = None,
+) -> bytes:
     """fastapiのテストコード用。
 
     Args:
         response: レスポンス
         status_code: 期待するステータスコード
+        content_type: 期待するContent-Type
 
     Raises:
         AssertionError: ステータスコードが異なる場合
@@ -25,20 +34,25 @@ def assert_bytes(response: httpx.Response, status_code: int = 200) -> bytes:
     """
     response_body = response.content
 
-    # ステータスコードチェック
-    if response.status_code != status_code:
-        logger.info(
-            f"ステータスコードエラー: {response.status_code} != {status_code}\n\n{response_body!r}"
-        )
-        raise AssertionError(
-            f"ステータスコードエラー: {response.status_code} != {status_code})"
-        )
+    try:
+        # ステータスコードチェック
+        pytilpack.web.check_status_code(response.status_code, status_code)
+
+        # Content-Typeチェック
+        pytilpack.web.check_content_type(response.content_type, content_type)
+    except AssertionError as e:
+        logger.info(f"{e}\n\n{response_body!r}")
+        raise e
 
     return response_body
 
 
 def assert_html(
-    response: httpx.Response, status_code: int = 200, strict: bool = False
+    response: httpx.Response,
+    status_code: int = 200,
+    content_type: str | typing.Iterable[str] | None = "__default__",
+    tmp_path: pathlib.Path | None = None,
+    strict: bool = False,
 ) -> str:
     """fastapiのテストコード用。
 
@@ -47,6 +61,9 @@ def assert_html(
     Args:
         response: レスポンス
         status_code: 期待するステータスコード
+        content_type: 期待するContent-Type
+        tmp_path: 一時ファイルを保存するディレクトリ
+        strict: HTML解析を厳格に行うかどうか
 
     Raises:
         AssertionError: ステータスコードが異なる場合
@@ -59,34 +76,41 @@ def assert_html(
 
     response_body = response.text
 
-    # HTMLのチェック
-    parser = html5lib.HTMLParser(strict=strict, debug=True)
     try:
-        _ = parser.parse(io.BytesIO(response.content))
-    except html5lib.html5parser.ParseError as e:
-        logger.info(f"HTMLエラー: {e}\n\n{response_body}")
-        raise AssertionError(f"HTMLエラー: {e}") from e
+        # ステータスコードチェック
+        pytilpack.web.check_status_code(response.status_code, status_code)
 
-    # ステータスコードチェック
-    if response.status_code != status_code:
-        logger.info(
-            f"ステータスコードエラー: {response.status_code} != {status_code}\n\n{response_body}"
+        # Content-Typeチェック
+        if content_type == "__default__":
+            content_type = ["text/html", "application/xhtml+xml"]
+        pytilpack.web.check_content_type(response.content_type, content_type)
+
+        # HTMLのチェック
+        parser = html5lib.HTMLParser(strict=strict, debug=True)
+        try:
+            _ = parser.parse(io.BytesIO(response.content))
+        except html5lib.html5parser.ParseError as e:
+            raise AssertionError(f"HTMLエラー: {e}") from e
+    except AssertionError as e:
+        tmp_file_path = pytilpack.pytest_.create_temp_view(
+            tmp_path, response_body, ".html"
         )
-        raise AssertionError(
-            f"ステータスコードエラー: {response.status_code} != {status_code})"
-        )
+        raise AssertionError(f"{e} (HTML: {tmp_file_path} )") from e
 
     return response_body
 
 
 def assert_json(
-    response: httpx.Response, status_code: int = 200
+    response: httpx.Response,
+    status_code: int = 200,
+    content_type: str | typing.Iterable[str] | None = "application/json",
 ) -> dict[str, typing.Any]:
     """fastapiのテストコード用。
 
     Args:
         response: レスポンス
         status_code: 期待するステータスコード
+        content_type: 期待するContent-Type
 
     Raises:
         AssertionError: ステータスコードが異なる場合
@@ -97,13 +121,20 @@ def assert_json(
     """
     response_body = response.text
 
-    # ステータスコードチェック
-    if response.status_code != status_code:
-        logger.info(
-            f"ステータスコードエラー: {response.status_code} != {status_code}\n\n{response_body}"
-        )
-        raise AssertionError(
-            f"ステータスコードエラー: {response.status_code} != {status_code})"
-        )
+    try:
+        # ステータスコードチェック
+        pytilpack.web.check_status_code(response.status_code, status_code)
 
-    return response.json()
+        # Content-Typeチェック
+        pytilpack.web.check_content_type(response.content_type, content_type)
+
+        # JSONのチェック
+        try:
+            data = response.json()
+        except Exception as e:
+            raise AssertionError(f"JSONエラー: {e}") from e
+    except AssertionError as e:
+        logger.info(f"{e}\n\n{response_body!r}")
+        raise e
+
+    return data
