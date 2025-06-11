6d9cb377d134e74b6d8c64fa5e08ed8daa1971c3
aki. <mark@aur.ll.to>
2025-06-11 20:42:54 +0900

    .

--- pytilpack/tiktoken_.py
+++ pytilpack/tiktoken_.py
@@ -6,6 +6,7 @@ import logging
 import math
 import re
 import typing
+import warnings
 
 import httpx
 import openai.types.chat
@@ -29,6 +30,7 @@ def num_tokens_from_messages(
     model: str,
     messages: list[openai.types.chat.ChatCompletionMessageParam],
     tools: list[openai.types.chat.ChatCompletionToolParam] | None = None,
+    tool_choice: openai.types.chat.ChatCompletionNamedToolChoiceParam | None = None,
 ) -> int:
     """メッセージからトークン数を算出。
 
@@ -38,59 +40,17 @@ def num_tokens_from_messages(
         model: モデル名。
         messages: メッセージのリスト。
         tools: function callingの情報。
+        tool_choice: ツール選択の情報。
 
     Returns:
         トークン数。
 
     """
     encoding = get_encoding_for_model(model)
-    if model in {
-        "gpt-3.5-turbo-0613",
-        "gpt-3.5-turbo-16k-0613",
-        "gpt-3.5-turbo-1106",
-        "gpt-3.5-turbo-0125",
-        "gpt-4-0314",
-        "gpt-4-32k-0314",
-        "gpt-4-0613",
-        "gpt-4-32k-0613",
-        "gpt-4-1106-preview",
-        "gpt-4-0125-preview",
-        "gpt-4-vision-preview",
-        "gpt-4-turbo-2024-04-09",
-        "gpt-4o-mini-2024-07-18",
-        "gpt-4o-2024-05-13",
-        "gpt-4o-2024-08-06",
-    }:
-        tokens_per_message = 3
-        tokens_per_name = 1
 
-    elif "gpt-3.5-turbo" in model:
-        logger.warning(
-            "Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0125."
-        )
-        return num_tokens_from_messages("gpt-3.5-turbo-0125", messages, tools)
-    elif "gpt-4o-mini" in model:
-        logger.warning(
-            "Warning: gpt-4o-mini may update over time. Returning num tokens assuming gpt-4o-mini-2024-07-18."
-        )
-        return num_tokens_from_messages("gpt-4o-mini-2024-07-18", messages, tools)
-    elif "gpt-4o" in model:
-        logger.warning(
-            "Warning: gpt-4o and gpt-4o-mini may update over time. Returning num tokens assuming gpt-4o-2024-08-06."
-        )
-        return num_tokens_from_messages("gpt-4o-2024-08-06", messages, tools)
-    elif "gpt-4" in model:
-        logger.warning(
-            "Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613."
-        )
-        return num_tokens_from_messages("gpt-4-0613", messages, tools)
-    else:
-        logger.error(
-            f"num_tokens_from_messages() is not implemented for model {model}."
-            " See https://github.com/openai/openai-python/blob/main/chatml.md"
-            " for information on how messages are converted to tokens."
-        )
-        return num_tokens_from_messages("gpt-3.5-turbo-0125", messages, tools)
+    tokens_per_message = 3
+    tokens_per_name = 1
+
     num_tokens = 0
     for message in messages:
         num_tokens += tokens_per_message
@@ -127,7 +87,7 @@ def num_tokens_from_messages(
     num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
 
     if tools is not None:
-        num_tokens += num_tokens_from_tools(encoding, tools)
+        num_tokens += num_tokens_for_tools(model, tools, tool_choice, encoding=encoding)
 
     return num_tokens
 
@@ -194,11 +154,49 @@ def num_tokens_from_texts(model: str, texts: list[str] | str) -> int:
 def num_tokens_from_tools(
     encoding: tiktoken.Encoding, tools: list[openai.types.chat.ChatCompletionToolParam]
 ) -> int:
+    warnings.warn(
+        "num_tokens_from_tools is deprecated. Use num_tokens_for_tools instead.",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    return num_tokens_for_tools(model="gpt-4o", tools=tools, encoding=encoding)
+
+
+def num_tokens_for_tools(
+    model: str,
+    tools: list[openai.types.chat.ChatCompletionToolParam],
+    tool_choice: openai.types.chat.ChatCompletionNamedToolChoiceParam | None = None,
+    encoding: tiktoken.Encoding | None = None,
+) -> int:
     """Function calling部分のトークン数算出。
 
+    <https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb>
     <https://community.openai.com/t/how-to-calculate-the-tokens-when-using-function-call/266573/10>
 
     """
+    if len(tools) == 0:
+        return 0
+
+    if encoding is None:
+        encoding = get_encoding_for_model(model)
+
+    if "gpt-3.5-" in model or "gpt-4-" in model or model == "gpt-4":
+        # gpt-3.5-turbo, gpt-4, gpt-4-turboまで
+        func_init = 10
+        prop_init = 3
+        prop_key = 3
+        enum_init = -3
+        enum_item = 3
+        func_end = 12
+    else:
+        # gpt-4o, o1など以降
+        func_init = 7
+        prop_init = 3
+        prop_key = 3
+        enum_init = -3
+        enum_item = 3
+        func_end = 12
+
     num_tokens = 0
     for tool in tools:
         if tool.get("type") != "function":
@@ -208,35 +206,51 @@ def num_tokens_from_tools(
             continue
         function = tool["function"]
         try:
-            function_tokens = len(encoding.encode(function["name"]))
-            function_tokens += len(encoding.encode(function["description"]))
+            num_tokens += func_init
+            print(f"{num_tokens=} func_init")
+
+            func_name = function["name"]
+            func_desc = function["description"]
+            if func_desc.endswith("."):
+                func_desc = func_desc[:-1]
+            num_tokens += len(encoding.encode(f"{func_name}:{func_desc}"))
+            print(f"{num_tokens=} func")
 
             parameters = function["parameters"]
             properties = typing.cast(
                 dict[str, dict[str, typing.Any]], parameters.get("properties", {})
             )
             if len(properties) > 0:
-                for k, v in properties.items():
-                    function_tokens += len(encoding.encode(k))
-                    for field, field_value in v.items():
-                        if field == "type":
-                            function_tokens += 2
-                            function_tokens += len(encoding.encode(field_value))
-                        elif field == "description":
-                            function_tokens += 2
-                            function_tokens += len(encoding.encode(field_value))
-                        elif field == "enum":
-                            function_tokens -= 3
-                            for o in field_value:
-                                function_tokens += 3
-                                function_tokens += len(encoding.encode(o))
-                        else:
-                            logger.warning(f"not supported field {field}")
-                function_tokens += 11
+                num_tokens += prop_init
+                print(f"{num_tokens=} prop_init")
+                for prop_name, fields in properties.items():
+                    num_tokens += prop_key
 
-            num_tokens += function_tokens
+                    prop_type = fields.get("type", "")
+                    prop_desc = fields.get("description", "")
+                    num_tokens += len(
+                        encoding.encode(f"{prop_name}:{prop_type}:{prop_desc}")
+                    )
+
+                    if "enum" in fields:
+                        num_tokens += enum_init
+                        for item in fields["enum"]:
+                            num_tokens += enum_item
+                            num_tokens += len(encoding.encode(item))
+
         except Exception:
             logger.exception("Failed to calculate tokens from tools.", exc_info=True)
+    num_tokens += func_end
 
-    num_tokens += 12
+    # If tool_choice is 'none', add one token.
+    # If it's an object, add 4 + the number of tokens in the function name.
+    # If it's undefined or 'auto', don't add anything.
+    if tool_choice == "none":
+        num_tokens += 1
+    elif isinstance(tool_choice, dict):
+        num_tokens += 7
+        num_tokens += len(
+            encoding.encode(str(tool_choice.get("function", {}).get("name", "")))
+        )
+
     return num_tokens
