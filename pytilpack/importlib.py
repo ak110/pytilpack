2aae841ab0e06c15d109fe4831da6734ee1ba18a
aki. <mark@aur.ll.to>
2025-07-20 23:30:52 +0900

    .

--- pytilpack/importlib.py
+++ pytilpack/importlib.py
@@ -12,7 +12,7 @@ def import_all(path: pathlib.Path, base_path: pathlib.Path | None = None) -> Non
     if base_path is None:
         base_path = path
 
-    for item in path.rglob("*.py"):
+    for item in sorted(path.rglob("*.py")):
         import_path = item.parent if item.name == "__init__.py" else item.with_suffix("")
         module_name = ".".join(import_path.relative_to(base_path).parts)
         logger.debug(f"Importing module: {module_name}")
